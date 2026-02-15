ef9335ab5d429dc0ecee4b2475e8f778f1a5ef4f
ChethiyaKorala <chethiyakorala@gmail.com>
2026-02-15 14:14:36 +0530

    Update index.js

--- index.js
+++ index.js
@@ -4,12 +4,13 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.get("/", (req, res) => {
-  res.send("This is patch fix");
+  res.send("This is patch fix double");
 });
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
+
 
 
 
