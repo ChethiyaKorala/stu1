465d744865121df0612597d4cf33941f3b41fef1
ChethiyaKorala <chethiyakorala@gmail.com>
2026-02-17 15:40:03 +0530

    Change response message in root route
    Updated the response message in the root route.

--- index.js
+++ index.js
@@ -4,12 +4,13 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.get("/", (req, res) => {
-  res.send("Ts patchbhf jfdfd kknj    nn  jj ");
+  res.send("Here I come from the GitHub ");
 });
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
+
 
 
 
