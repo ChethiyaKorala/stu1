c6372cadada863bd317e400892caed622b9f74e1
ChethiyaKorala <chethiyakorala@gmail.com>
2026-02-17 12:49:35 +0530

    Update index.js

--- index.js
+++ index.js
@@ -4,12 +4,13 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.get("/", (req, res) => {
-  res.send("Ts patchbj dub boah hu  fllo ");
+  res.send("Ts patchbj dub boahfllo ");
 });
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
+
 
 
 
