6f3505e75639b70d475a214705d5c24fb26dfe82
ChethiyaKorala <chethiyakorala@gmail.com>
2026-02-17 15:49:15 +0530

    Update index.js

--- index.js
+++ index.js
@@ -4,12 +4,13 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.get("/", (req, res) => {
-  res.send("Here I come from the GitHub");
+  res.send("Here I come from the GitHub world");
 });
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
+
 
 
 
